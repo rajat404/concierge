35c072c7cedba35bb15c67cb890839baa172cdf7
rajat404 <404rajat@gmail.com>
2017-07-17 13:47:23 +0530

    render template & api based on format

--- concierge/event/api.py
+++ concierge/event/api.py
@@ -5,6 +5,7 @@ from concierge.base.api.viewsets import NoDeleteModelViewSet
 from .models import Event, Speaker
 from .serializers import EventSerializer, SpeakerSerializer
 from .services import create_event
+from rest_framework.response import Response
 
 
 class SpeakerViewset(NoDeleteModelViewSet):
@@ -15,8 +16,15 @@ class SpeakerViewset(NoDeleteModelViewSet):
 class EventViewset(NoDeleteModelViewSet):
     queryset = Event.objects.order_by('id')
     serializer_class = EventSerializer
+    template_name = 'event_list.html'
 
     def create(self, request, *args, **kwargs):
         serializer = self.get_serializer(data=request.data)
         create_event(serializer)
         return rsp.Created(serializer.data)
+
+    def list(self, request, *args, **kwargs):
+        if request.accepted_renderer.format == 'html':
+            return Response({'events': self.queryset})
+        else:
+            return super().list(request, *args, **kwargs)
